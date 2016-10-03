2a428102f3657de5a46ead6ec0ce47b51c0618fa
Maxim Soldatov <ms@nowmedia.ru>
2016-10-03 18:42:59 +0300

    implement ParseQuery::__get

--- ParseQuery.php
+++ ParseQuery.php
@@ -93,6 +93,22 @@ class ParseQuery extends XPathQuery
 		return ($node = $this->get(0)) ? $node->ownerDocument->saveHTML($node) : null;
 	}
 
+	public function __get($name)
+	{
+		if (method_exists($this, $name))
+			return $this->$name();
+
+		if ($node = $this->get(0)) {
+			if (isset($node->$name))
+				return $node->$name;
+
+			if ($node->hasAttribute($name))
+				return $node->getAttribute($name);
+		}
+
+		return null;
+	}
+
 	public function __toString()
 	{
 		return '['.implode(', ', array_map(function($node){
